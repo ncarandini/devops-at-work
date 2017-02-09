54a54de4cd35d3720afa07d0cb0027dbcc9ab6f8
Nicolò Carandini <n.carandini@outlook.com>
2017-02-09 22:44:33 +0100

    Add versioning to ValueController

--- DevOpsAtWork.DemoWebApp/Controllers/ValuesController.cs
+++ DevOpsAtWork.DemoWebApp/Controllers/ValuesController.cs
@@ -6,21 +6,57 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DevOpsAtWork.DemoWebApp.Controllers
 {
-    [Route("api/[controller]")]
-    public class ValuesController : Controller
+    [Route("api/v1/values")]
+    public class V1_ValuesController : Controller
     {
         // GET api/values
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Version 1 - value1", "Version 1 - value2" };
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            return $"Version 1 - value {id.ToString()}";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+
+    [Route("api/v2/values")]
+    public class V2_ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Version 2 - value1", "Version 2 - value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return $"Version 2 - value {id.ToString()}";
         }
 
         // POST api/values
@@ -42,3 +78,4 @@ namespace DevOpsAtWork.DemoWebApp.Controllers
         }
     }
 }
+
